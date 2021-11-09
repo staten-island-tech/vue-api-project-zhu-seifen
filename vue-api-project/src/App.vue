7a7dec6754cc3208fcf914f3b826c60226f551bd
IvyZ23 <ivy.zhu23@sitechhs.com>
2021-11-09 12:20:07 -0500

    Update App.vue

--- vue-api-project/src/App.vue
+++ vue-api-project/src/App.vue
@@ -8,6 +8,33 @@
   </div>
 </template>
 
+<script>
+export default {
+  data(){
+    return {
+      events: [],
+      
+  }
+},
+created: function() {
+  this.fetchData();
+},
+methods: {
+
+  fetchData: async function(){
+              try {
+                const result = await fetch(`https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=usCi4RaBNDKBfG3jWXiTgwjpfSJ6aWG4`)
+                const data = await result.json();
+                console.log(data)
+              } catch (error) {
+                console.log(error)
+              }
+            }
+},
+}
+
+</script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
