0093180a20703af180ce8f1279657392bd24654f
IvyZ23 <ivy.zhu23@sitechhs.com>
2021-11-09 12:28:56 -0500

    -

--- vue-api-project/src/views/Home.vue
+++ vue-api-project/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section class="header">
+      <div class="heading">
+        <span class="title">New York Times</span>
+        <span class="title2">Best Sellers</span>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    
   },
 };
 </script>
