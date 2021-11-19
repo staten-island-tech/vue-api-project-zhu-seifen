084b312647931a67fb491a6a3c240afbdd8c59f3
IvyZ23 <ivy.zhu23@sitechhs.com>
2021-11-19 12:27:40 -0500

    -

--- vue-api-project/src/views/Home.vue
+++ vue-api-project/src/views/Home.vue
@@ -44,9 +44,6 @@ export default {
       try {
         const result = await fetch(`https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=usCi4RaBNDKBfG3jWXiTgwjpfSJ6aWG4`)
         const data = await result.json();
-        const result2 = await fetch(`https://api.nytimes.com/svc/books/v3/reviews.json?api-key=usCi4RaBNDKBfG3jWXiTgwjpfSJ6aWG4`)
-        const data2 = await result2.json();
-        console.log(data2)
         this.lists = data
         this.books = this.lists.results.lists[this.index]
         console.log(this.books)    
@@ -93,8 +90,7 @@ export default {
 .header {
     height: 92vh;
     background-image:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-url("../assets/shelves.jpg");
-
+    url("../assets/shelves.jpg");
     display:flex;
     background-repeat: no-repeat;
     background-size: cover;
--- vue-api-project/src/views/Search.vue
+++ vue-api-project/src/views/Search.vue
@@ -1,23 +1,81 @@
 <template>
     <div id="search-page">
-        <form class="search-form">
+        <h2>Search for a book!</h2>
+        <form class="search-form" @submit.prevent="onSubmit">
             <input 
             class="search-bar" 
             type="text" 
-            placeholder="Search for the book's title...">
+            placeholder="Search for the book's title..."
+            v-model="searchParams">
             <input class="submit-btn" type="submit">
         </form>
     </div>
 </template>
 
 <script>
-
 export default {
-
+    data() {
+        return {
+            searchParams: null,
+        }
+    },
+    created: function() {
+        this.fetchData();
+    },
+    methods: {
+        onSubmit() {
+            console.log(this.searchParams)
+        },
+        fetchData: async function(){
+            try {
+                const result = await fetch(`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?title=apple&api-key=usCi4RaBNDKBfG3jWXiTgwjpfSJ6aWG4`)
+                const data = await result.json();
+                console.log(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },
 }
 </script>
 
 <style scoped>
+    #search-page {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 45vh;
+        background-image:linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+        url("../assets/books.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        flex-direction: column;
+    }
 
+    .search-bar {
+        width: 15rem;
+        height: 1.5rem;
+        border: none;
+        border-bottom: #FF9800 solid 2px;
+    }
+
+    .submit-btn {
+        background-color: #FF9800;
+	    border: #FF9800 2px solid;
+        color: white;
+        padding: .3rem;
+	    cursor: pointer;
+	    outline: inherit;
+        transition: all .5s ease-out;
+        width: 5rem;
+        margin-left: .2rem;
+        border-bottom: 2px solid #E64A19;
+    }
+
+    h2{
+        color: white;
+        font-size: 1.3rem;
+        font-weight: 100;
+    }
 </style>
 
