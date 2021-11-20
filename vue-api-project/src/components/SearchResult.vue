ecc6faec02f6d498ac9f9785ac59762c4742cef9
IvyZ23 <ivy.zhu23@sitechhs.com>
2021-11-20 13:59:11 -0500

    -

---
+++ vue-api-project/src/components/SearchResult.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="search-result">
+      <div id="result-info">
+        <h2> {{ result.title }} </h2>
+        <p> {{ result.author }} </p>
+        <p> {{ result.description }} </p>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: "SearchResult",
+    props: {
+        result: Object,
+    },
+}
+</script>
+
+<style scoped>
+    .search-result {
+        width: 30vw;
+        border: gray 1px solid;
+        border-radius: 5px;
+        margin: 1rem;
+    }
+
+    #result-info {
+        padding: 2rem;
+    }
+
+    h2 {
+        font-size: 1.5rem;
+    }
+
+    p {
+        font-size: 1rem;
+    }
+</style>
--- vue-api-project/src/views/Search.vue
+++ vue-api-project/src/views/Search.vue
@@ -1,40 +1,85 @@
 <template>
-    <div id="search-page">
-        <h2>Search for a book!</h2>
-        <form class="search-form" @submit.prevent="onSubmit">
-            <input 
-            class="search-bar" 
-            type="text" 
-            placeholder="Search for the book's title..."
-            v-model="searchParams">
-            <input class="submit-btn" type="submit">
-        </form>
+    <div id="search">
+        <section id="search-page">
+            <h2>Search for a book!</h2>
+            <form class="search-form" @submit.prevent="fetchData">
+                <input 
+                class="search-bar" 
+                type="text" 
+                placeholder="Search for the book's title..."
+                v-model="searchParams">
+                <input class="submit-btn" type="submit">
+            </form>
+        </section>
+        <section id="results">
+            <div>
+                <p :class="{ hide: validParam }">{{ message }}</p>
+            </div>
+            <div class="searchResults">
+                <SearchResult v-for="result in results" :key="result.title" :result="result" :class="{ hide: !validParam }"/>
+               
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
+import SearchResult from "../components/SearchResult.vue";
+
 export default {
+    name: "Search",
+    components: {
+        SearchResult
+    },
     data() {
         return {
             searchParams: null,
+            data: [],
+            results: ["result"],
+            message: "Search to see results!",
         }
     },
     created: function() {
         this.fetchData();
     },
     methods: {
-        onSubmit() {
-            console.log(this.searchParams)
-        },
         fetchData: async function(){
             try {
-                const result = await fetch(`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?title=apple&api-key=usCi4RaBNDKBfG3jWXiTgwjpfSJ6aWG4`)
+                const result = await fetch(`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?title=${this.searchParams}&api-key=usCi4RaBNDKBfG3jWXiTgwjpfSJ6aWG4`)
                 const data = await result.json();
-                console.log(data)
+                this.data = data
+                this.results = data.results
+                this.onSubmit()
+                console.log(this.results)
             } catch (error) {
                 console.log(error)
+                
             }
+        },
+        onSubmit() {
+            if (this.searchParams.trim() === "" || this.data.num_results === 0) {
+                this.message = "Invalid search, please try again!"
+                this.searchParams = "";
+            } else {
+                this.searchParams = "";
+            }
+        },
+    },
+    computed: {
+        validParam() {
+        if (this.data.num_results === 0) {
+            return false   
+          } else {
+            return true
         }
+      },
+      /*  formEntered() {
+          if (this.searchParams.trim() === "") {
+              return false
+          } else {
+              return true
+          }
+      } */
     },
 }
 </script>
@@ -52,23 +97,28 @@ export default {
         flex-direction: column;
     }
 
+    .search-form {
+        width: 40%;
+        display:flex;
+        flex-direction: row;
+    }
+
     .search-bar {
-        width: 15rem;
-        height: 1.5rem;
+        width: 100%;
+        padding: .5rem 1rem;
         border: none;
         border-bottom: #FF9800 solid 2px;
     }
 
     .submit-btn {
         background-color: #FF9800;
-	    border: #FF9800 2px solid;
         color: white;
-        padding: .3rem;
+        padding: .4rem;
 	    cursor: pointer;
 	    outline: inherit;
-        transition: all .5s ease-out;
         width: 5rem;
         margin-left: .2rem;
+        border:none;
         border-bottom: 2px solid #E64A19;
     }
 
@@ -76,6 +126,19 @@ export default {
         color: white;
         font-size: 1.3rem;
         font-weight: 100;
+    }
+
+    .hide {
+        display: none;
+    }
+
+    p {
+        font-size: 1rem;
+    }
+
+    .searchResults {
+        display: flex;
+        flex-wrap: wrap;
     }
 </style>
 
